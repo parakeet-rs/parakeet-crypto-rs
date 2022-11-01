497498754ce45e486f34f4b0a3b4366dedbf1666
Jixun Wu <jixun.moe@gmail.com>
2022-11-01 18:57:02 +0000

    feat: implement xmly interface

--- src/ximalaya/xmly_crypto.rs
+++ src/ximalaya/xmly_crypto.rs
@@ -1,26 +1,69 @@
+use std::io::SeekFrom;
+
+use crate::interfaces::decryptor::{Decryptor, DecryptorError, SeekReadable};
+
+/// Why "generic" instead of a vector etc:
+///   The key size is known to be the power of twos (4 & 32).
+///   By using a fixed size, that is known at compile time,
+///   the "mod" opcode can be optimised as "bitwise and" instead.
+///   (Performance reasons)
 #[derive(Debug, Clone, Copy)]
-struct XmlyCrypto<const KEY_SIZE: usize> {
-    crypto_key: [u8; KEY_SIZE],
+pub struct XmlyCrypto<const KEY_SIZE: usize> {
+    content_key: [u8; KEY_SIZE],
     scramble_table: [usize; 1024],
 }
 
-impl<const KEY_SIZE: usize> Default for XmlyCrypto<KEY_SIZE> {
-    fn default() -> Self {
+impl<const KEY_SIZE: usize> XmlyCrypto<KEY_SIZE> {
+    pub fn new(content_key: &[u8; KEY_SIZE], scramble_table: &[usize; 1024]) -> Self {
         Self {
-            crypto_key: [0; KEY_SIZE],
-            scramble_table: [0; 1024],
+            content_key: *content_key,
+            scramble_table: *scramble_table,
         }
     }
-}
 
-impl<const KEY_SIZE: usize> XmlyCrypto<KEY_SIZE> {
     fn decrypt_header(&self, &buffer: &[u8; 1024]) -> [u8; 1024] {
         let mut result = buffer;
 
         for (key_index, &i) in self.scramble_table.iter().enumerate() {
-            result[i] = buffer[i] ^ self.crypto_key[key_index % self.crypto_key.len()]
+            result[i] = buffer[i] ^ self.content_key[key_index % self.content_key.len()]
         }
 
         result
     }
+}
+
+impl<const KEY_SIZE: usize> Decryptor for XmlyCrypto<KEY_SIZE> {
+    fn check(&self, _from: &mut dyn SeekReadable) -> Result<bool, DecryptorError> {
+        // TODO: Verify decrypted header after implementing AudioHeader checker.
+        Ok(true)
+    }
+
+    fn decrypt(
+        &self,
+        from: &mut dyn SeekReadable,
+        to: &mut dyn std::io::Write,
+    ) -> Result<(), DecryptorError> {
+        let mut header = [0u8; 1024];
+
+        from.seek(SeekFrom::Start(0))
+            .or(Err(DecryptorError::IOError))?;
+
+        from.read_exact(&mut header)
+            .or(Err(DecryptorError::IOError))?;
+
+        let header = self.decrypt_header(&header);
+        to.write_all(&header).or(Err(DecryptorError::IOError))?;
+
+        std::io::copy(from, to).or(Err(DecryptorError::IOError))?;
+
+        Ok(())
+    }
+}
+
+pub fn new_x2m(content_key: &[u8; 4], scramble_table: &[usize; 1024]) -> XmlyCrypto<4> {
+    XmlyCrypto::new(content_key, scramble_table)
+}
+
+pub fn new_x3m(content_key: &[u8; 32], scramble_table: &[usize; 1024]) -> XmlyCrypto<32> {
+    XmlyCrypto::new(content_key, scramble_table)
 }
